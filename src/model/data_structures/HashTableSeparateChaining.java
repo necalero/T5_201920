8a706ba4c7cc77bccdc5866c7b4fa9bd8562c44e
unknown <ne.calero@uniandes.edu.co>
2019-10-09 23:56:34 -0500

    Se implementa la tabla de hash con Linear Probing

--- src/model/data_structures/HashTableSeparateChaining.java
+++ src/model/data_structures/HashTableSeparateChaining.java
@@ -3,10 +3,14 @@ package model.data_structures;
 public class HashTableSeparateChaining<T extends Comparable<T>> 
 {
 	int capacidad;
+	int[] keys;
+	Nodo[] data;
 	
 	public HashTableSeparateChaining(int m)
 	{
 		capacidad = m;
+		keys = new int[m];
+		data = new Nodo[m];
 	}
 	
 
