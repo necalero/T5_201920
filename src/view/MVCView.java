2d544b776395cd1a7b783c7a15038245bb73df9f
unknown <ne.calero@uniandes.edu.co>
2019-10-10 00:15:13 -0500

    Se actualiza el menu de MVCView

--- src/view/MVCView.java
+++ src/view/MVCView.java
@@ -14,8 +14,9 @@ public class MVCView
 	    
 		public void printMenu()
 		{
-			System.out.println("1. Cargar viajes por un trimestre seleccionado (1 o 2)");
-			System.out.println("2. Generar muestra de datos");
+			System.out.println("1. Cargar viajes por un trimestre seleccionado (1, 2, 3 o 4)");
+			System.out.println("2. Buscar tiempos de viaje por trimestre, zona de origen y zona de destino. (Linear Probing)");
+			System.out.println("3. Buscar tiempos de viaje por trimestre, zona de origen y zona de destino. (Separate Chaining)");
 			
 			System.out.println("Dar el numero de opcion a resolver, luego oprimir tecla Return:");
 		}
